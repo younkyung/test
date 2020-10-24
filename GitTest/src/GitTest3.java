1a55693df9f536c518ba58d93eed5864e6445733
younkyung <lyk9011@gmail.com>
2020-10-24 13:05:20 +0900

    GitTest3 update

--- GitTest/src/GitTest3.java
+++ GitTest/src/GitTest3.java
@@ -4,7 +4,7 @@ public class GitTest3 {
 	public static void main(String[] args) {
 		String ThirdGit;
 		
-		ThirdGit = "ThirdGit";
+		ThirdGit = "ThirdGit_test";
 		
 		System.out.println(ThirdGit);
 
